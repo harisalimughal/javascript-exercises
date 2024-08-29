d88f3bff08747098eafe24875d0ad1a5056fb640
Haris <harisalimughal077@gmail.com>
2024-08-29 19:18:50 +0500

    RemoveFromArray exercise Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,73 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(arr,num1) {
+//     let arr1=arr.slice(0,num1-1);
+//     let arr2=arr.slice(num1);
+//     let arr3=arr1.concat(arr2);
 
+//     return arr3;
+
+    
+// };
+
+// const removeFromArray = function(arr,n1,n2) {
+//     const arrwithoutN = [];
+//   for(i=0;i<=arr.length;i++){
+//     if(arr[i]!== n1 && arr[i]!== n2){
+//         arrwithoutN.push(arr[i]);
+//     }
+//   }
+//     return arrwithoutN;
+
+    
+// };
+
+// const removeFromArray = function(arr,n1) {
+//     const arrwithoutN = [];
+//   for(i=0;i<=arr.length;i++){
+//     if(arr[i]!== n1 ){
+//         arrwithoutN.push(arr[i]);
+//     }
+//   }
+//     return arrwithoutN;
+
+    
+// };
+
+// const removeFromArray = function(arr,n1,n2) {
+//     const arrwithoutN = [];
+//   for(i=0;i<=arr.length;i++){
+//     if(arr[i]!== n1 && arr[i]!== n2){
+//         arrwithoutN.push(arr[i]);
+//     }
+//   }
+//     return arrwithoutN;
+
+    
+// };
+
+// const removeFromArray = function(arr,n1,n2,n3,n4) {
+//     const arrwithoutN = [];
+//   for(i=0;i<=arr.length;i++){
+//     if(arr[i]!== n1 && arr[i]!== n2 && arr[i]!== n3 && arr[i]!== n4){
+//         arrwithoutN.push(arr[i]);
+//     }
+//   }
+//     return arrwithoutN;
+
+    
+// };
+
+const removeFromArray = function(arr, ...valuestoRemove) {
+    const arrwithoutN = [];
+  for(i=0; i<arr.length; i++){
+        if(!valuestoRemove.includes(arr[i])){
+            arrwithoutN.push(arr[i]);
+        }
+
+    
+  }
+    return arrwithoutN;
+
+    
+};
 // Do not edit below this line
 module.exports = removeFromArray;
